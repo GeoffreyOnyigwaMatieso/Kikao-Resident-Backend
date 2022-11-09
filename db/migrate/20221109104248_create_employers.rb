806ffff31079885d8234ff459d064495a397e203
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-09 13:55:19 +0300

    created Employers table

---
+++ db/migrate/20221109104248_create_employers.rb
@@ -0,0 +1,9 @@
+class CreateEmployeers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :employers do |t|
+      t.string :name
+      t.integer :EmployersID
+      t.string :Occupation
+    end
+  end
+end
