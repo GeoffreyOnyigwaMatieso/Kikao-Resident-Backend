924614fd0e003a7ab0960814c27e36450c14e803
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-10 11:41:12 +0300

    modified the schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_09_104248) do
+ActiveRecord::Schema.define(version:2022_11_10_083045) do
 
   create_table "employees", force: :cascade do |t|
     t.string "name"
