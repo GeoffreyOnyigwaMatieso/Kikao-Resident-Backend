cb35341fef79fd63a853bd9ba220736ac29239b4
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-10 11:38:50 +0300

    manipulated table

--- db/migrate/20221110083117_create_employers.rb
+++ db/migrate/20221110083117_create_employers.rb
@@ -1,4 +1,9 @@
 class CreateEmployers < ActiveRecord::Migration[6.1]
   def change
+    create_table :employers do |t|
+      t.string :name
+      t.integer :employee_id
+      t.string :occupation
   end
+end
 end
