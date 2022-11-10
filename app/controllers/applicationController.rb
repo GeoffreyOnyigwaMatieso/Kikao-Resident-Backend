d5b9a572f349b898dbabaa1fd89f35a36e69617a
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-10 15:02:32 +0300

    fixed creating land

--- app/controllers/applicationController.rb
+++ app/controllers/applicationController.rb
@@ -2,59 +2,15 @@ class ApplicationController < Sinatra::Base
 
     set :default_content_type, 'application/json'
 
-    #STUDENT RELATIONS-COME FIRST FOR CRUD
-    get '/students' do
-        students = Student.all
-        students.to_json(only: [:id, :name, :class_name],
-             include: { course: { only: [:name, :study_type]}})
-    end
+     # employers table
+  get '/employers' do
+    employers = Employers.all.order(:name)
+    employers.to_json
+  end
 
-    post '/students' do
-        student = Student.create(
-          name: params[:name],
-          adm_no: params[:adm_no],
-          class_name: params[:class_name],
-          email: params[:email],
-          course_id: params[:course_id]
-        )
-        student.to_json
-    end
-
-    patch '/students/:id' do
-        student = Student.find(params[:id])
-        student.update(
-          class_name: params[:class_name],
-        )
-        student.to_json
-    end
-
-    delete '/students/:id' do
-        student = Student.find(params[:id])
-        student.destroy
-        student.to_json
-    end
-
-    #COURSES RELATION
-    get '/courses' do
-        course = Course.all
-        course.to_json
-    end
-
-    post '/courses' do
-        course = Course.create(
-          name: params[:name],
-          month: params[:month],
-          technology: params[:technology],
-          fees: params[:fees],
-          description: params[:description],
-          study_type: params[:study_type]
-        )
-        course.to_json
-    end
-
-    delete '/courses/:id' do
-        course = Course.find(params[:id])
-        course.destroy
-        course.to_json
-    end
+  #Search an employer
+  get '/employers/:id' do
+    employers = Employers.find(params[:id])
+    employers.to_json
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,50 @@ Employers.destroy_all
 Land.destroy_all
 Residents.destroy_all
 
+
+# items = []
+
+# for i in items
+#   Employers.create(
+#     name: Faker::Name.name,
+#     employers_id: rand(1..10)
+#   )
+#   Employees.create(
+#     name: Faker::Name.name,
+#     employees_id: rand(1..10)
+#   )
+#   Residents.create(
+#     name: Faker::Name.name,
+    
+#   )
+#   Land.create(
+#     location: Faker::Location.name
+#   )
+# end
+
+
 puts "Creating Employers"
-employers1 =Employers.create(name: "Baraka Baraza",employers_id:employers1.id,occupation:"Manager")
-employers2 =Employers.create(name: "Charline Nduva",employers_id:employers2.id,occupation:"Project Manager")
-employers3 =Employers.create(name: "Denzel Weloba",employers_id:employers3.id,occupation:"Human Resource")
+employers1 =Employers.create(name: "Baraka Baraza",employers_id: rand(1..10),occupation:"Manager")
+employers2 =Employers.create(name: "Charline Nduva",employers_id: rand(1..10),occupation:"Project Manager")
+employers3 =Employers.create(name: "Denzel Weloba",employers_id: rand(1..10),occupation:"Human Resource")
 
 
 puts "Creating Employees"
-employees1 =Employees.create(name: "Lorine Otieno",employee_id:employee1.id,occupation:"Cleaner")
-employees2 =Employees.create(name: "Michele njuguna",employee_id:employee2.id,occupation:"Welder")
-employees3 =Employees.create(name: "Timame Timame",employee_id:employee3.id,occupation:"Smith")
+employees1 =Employees.create(name: "Lorine Otieno",employee_id:rand(1..10),occupation:"Cleaner")
+employees2 =Employees.create(name: "Michele njuguna",employee_id:rand(1..10),occupation:"Welder")
+employees3 =Employees.create(name: "Timame Timame",employee_id:rand(1..10),occupation:"Smith")
 
 
 
-puts "Creating Residents"
-residents1 =Residents.create(name: "Lorine Otieno",residents_id1:residents1.id,occupation:"Cleaner")
-residents2 =Residents.create(name: "Michele njuguna",residents_id2:residents2.id,occupation:"Welder")
-residents3 =Residents.create(name: "Timame Timame",residents_id3:residents3.id,occupation:"Smith")
+# puts "Creating Residents"
+# residents1 =Residents.create(name: "Lorine Otieno",residents_id1:rand(1..10),occupation:"Cleaner")
+# residents2 =Residents.create(name: "Michele njuguna",residents_id2:rand(1..10),occupation:"Welder")
+# residents3 =Residents.create(name: "Timame Timame",residents_id3:rand(1..10),occupation:"Smith")
 
 
 puts "Creating Land"
 land1 = Land.create(
-    land_id:land_id1.id,
+    land_id:rand(1..10),
     location: "Mtito Andei",
     size:4567,
     land_use: "Housing",
@@ -35,19 +57,21 @@ land1 = Land.create(
     )
 
  land2 = Land.create(
-    land_id:land_id2.id,
+    land_id:rand(1..10),
     location: "Mtate Mwanza",
     size:4568,
     land_use: "Grazing",
     
     )
 land3 = Land.create(
-    land_id:land_id3.id,
+    land_id:rand(1..10),
     location: "Gikomba",
     size:4569,
     land_use: "Farming",
     
     )
+
+
 
 
 puts "🌱 Done seeding!"
